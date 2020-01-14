133615040abacd02df7fb8f228d46b0c0e78baa7
marisdanne <maris.danne@rvt.lv>
2020-01-14 09:20:52 +0000

    add active

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def getIndex():
 
 @app.route('/home')
 def getHome():
-  return render_template('home.html')
+  return render_template('home.html', active_page = 'home')
 
 @app.route('/about')
 def about():
