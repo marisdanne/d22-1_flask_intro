ea94be5ddeaf6c49b8f8667947aee7c10d8a3c50
Māris Danne <Maris.Danne@rvt.lv>
2020-02-03 11:35:39 +0200

    file_proc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -26,6 +27,18 @@ def params():
 @app.route('/post_req', methods = ['POST'])
 def post_req():
   return request.args
+
+@app.route('/read_file')
+def read_from_file():
+  file_content = read_file()
+  return file_content
+
+@app.route('/write_to_file', methods = ['POST'])
+def write_to_file():
+  line = request.form.get('data')
+  write_file(line)
+  return f"Add line '{line}' to file."
+
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", threaded=True, port=5050, debug=True) 
